5ca013628d0e2f591638418b1ce6580ab749e46c
liebeDY <hdydeveloper0927@gmail.com>
2020-11-25 21:28:55 +0900

    첫번째 커밋

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const config = require('./config/key')
+
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+// application/json
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://hdy20201004:tkfkdgoye0831@cluster0.c8utz.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected'))
   .catch(err => console.log('err'))
@@ -11,5 +20,20 @@ mongoose.connect('mongodb+srv://hdy20201004:tkfkdgoye0831@cluster0.c8utz.mongodb
 // mongodb+srv://hdy20201004:<password>@cluster0.c8utz.mongodb.net/<dbname>?retryWrites=true&w=majority
 
 app.get('/', (req, res) => res.send('Hello World'))
+
+app.post('/register', (req, res) => {
+    // 회원 가입 할 때 필요한 정보들을 client에서 가져오면 
+    // 그것들을 데이터 베이스에 넣어준다.
+
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
